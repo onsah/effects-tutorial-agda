c5cb6c9396d65d2d52664e93e54394d00b340470
Onur Sahin <sahinonur2000@hotmail.com>
2024-05-22 17:26:06 +0200

    chore: pin nixpkgs

--- shell.nix
+++ shell.nix
@@ -1,4 +1,7 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ 
+	nixpkgs ? fetchTarball "https://github.com/NixOS/nixpkgs/tarball/e381a1288138aceda0ac63db32c7be545b446921",
+}:
+let pkgs = import <nixpkgs> {}; in
 pkgs.mkShell {
 	buildInputs = with pkgs; [
 		# Package goes there
