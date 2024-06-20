d393ad0302fd517e6b724b7254afe70aa7cd85fb
Onur Sahin <sahinonur2000@hotmail.com>
2024-06-20 15:03:13 +0200

    chore: update shell.nix to use the pinned channel

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 { 
 	nixpkgs ? fetchTarball "https://github.com/NixOS/nixpkgs/tarball/e381a1288138aceda0ac63db32c7be545b446921",
 }:
-let pkgs = import <nixpkgs> {}; in
+let pkgs = import nixpkgs {}; in
 pkgs.mkShell {
 	buildInputs = with pkgs; [
 		# Package goes there
